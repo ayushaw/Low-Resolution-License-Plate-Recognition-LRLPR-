b8b37df9e517a31fc9fce54fd4906f99504fb03f
Kartikkdg05 <kartikdeepgautamkdg@gmail.com>
2026-02-28 23:47:18 +0530

    Add data loading function for training and testing

--- src/dataset.py
+++ src/dataset.py
@@ -1,1 +1,34 @@
+import os
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
 
+def get_dataloaders(data_dir, batch_size=32):
+
+    transform = transforms.Compose([
+        transforms.Resize((224,224)),
+        transforms.ToTensor()
+    ])
+
+    train_dataset = datasets.ImageFolder(
+        os.path.join(data_dir,"train"),
+        transform=transform
+    )
+
+    test_dataset = datasets.ImageFolder(
+        os.path.join(data_dir,"test"),
+        transform=transform
+    )
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True
+    )
+
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=False
+    )
+
+    return train_loader, test_loader
